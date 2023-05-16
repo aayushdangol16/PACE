79db482d1edac4bda17e411fcd7179a848b19aeb
Dipesh Thapa <thapadipesh2058@gmai.com>
2023-05-16 16:38:04 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const nav=document.querySelector('.mobile-navbar-btn');
+const nav_header=document.querySelector(".header");
+function togglebtn(){
+    
+}
+nav.addEventListener('click',()=>{
+    nav_header.classList.toggle('active');
+});
+
+
+
